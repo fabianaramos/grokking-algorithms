218d2eece916d834192c74b49c0b35da5a074b23
Fabiana Ramos <fabianavitoriaramos@gmail.com>
2024-07-08 21:50:32 -0300

    feat: add selection sort

---
+++ selection_sort.rb
@@ -0,0 +1,21 @@
+def search_min(arr)
+  min = arr[0]
+  min_index = 0
+
+  for i in 1...arr.size do
+    if arr[i] < min
+      min = arr[i]
+      min_index = i
+    end
+  end
+  min_index
+end
+
+def selection_sort(arr)
+  new_arr = []
+  for i in 1...arr.size do
+    min = search_min(arr)
+    new_arr.append(arr.delete_at(min))
+  end
+  new_arr
+end
